b0c4a11a5303b13208040f6bf1daf3b066de6ab4
Yuan.Chao <55234604+yuanchao614@users.noreply.github.com>
2023-05-04 16:20:45 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'react-qu-mobx-todo',
   plugins: [react()],
   build: {
    outDir: 'dist',
-   assetsDir: 'react-qu-mobx-todo/react-qu-mobx-todo',
+   assetsDir: 'react-qu-mobx-todo',
   }
 })
